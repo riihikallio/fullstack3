3c1cf9f0733e08dc91cb56292ef5a005a86a532c
Petri Riihikallio <petri.riihikallio@metis.fi>
2019-06-23 23:08:52 +0300

    uibuild

---
+++ build/precache-manifest.dd809e28b2291186a40743cd4997b073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8a8a7dfa73a8b3a57d8680e84c068a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9acf3e1fa116f99728c",
+    "url": "/static/css/main.d0d23d09.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "a9acf3e1fa116f99728c",
+    "url": "/static/js/main.9e693f38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
