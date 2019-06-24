ed5ef9c2b7d6bd9ab09bb3d424e24f52bc43cf76
Petri Riihikallio <petri.riihikallio@metis.fi>
2019-06-24 21:34:09 +0300

    3.18

--- index.js
+++ index.js
@@ -15,78 +15,64 @@ morgan.token('json', (req,res) => JSON.stringify(req.body))
 
 const Person = require('./models/person')
 
-const persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "432 432 432",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Clark Kent",
-        "number": "0400 654 655",
-        "id": 4
-    }
-]
-
 app.get('/', (req, res) => {
   res.send('<h1>Nothing to see, please move on..</h1>')
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date()}</p>`)
-  })
+app.get('/info', (req, res, next) => {
+  let count = 0
+  Person.find({})
+    .then(p =>  {
+      res.send(`<p>Phonebook has info for ${p.length} people</p>
+          <p>${new Date()}</p>`)
+    })
+    .catch(error => next(error))
+})
 
 app.get('/api/persons', (req, res) => {
   Person.find({})
     .then(ps => res.json(ps.map(p => p.toJSON())))
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
-      .then(p => res.json(p.toJSON))
+      .then(p => res.json(p.toJSON()))
       .catch(error => next(error))
-    // const id = Number(req.params.id)
-    // const person = persons.find(p => p.id === id)
-    // res.json(person)
-  })
+})
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-    .then(result => response.status(204).end())
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => res.status(204).end())
     .catch(error => next(error))
 })
 
 app.post('/api/persons/', (req, res) => {
     const body = req.body
     if (!(body.name && body.number)) {
-        return res.status(400).json({ 
-          error: 'name or number missing' 
-        })
-      }
-    if (persons.find(p => p.name.toLowerCase() === body.name.toLowerCase())) {
-        return res.status(400).json({ 
-            error: `${body.name} already exists` 
-          })        
+      return res.status(400).json({ 
+        error: 'name or number missing' 
+      })
     }
     const person = new Person({
-        name: body.name,
-        number: body.number,
-//      id: Math.floor(Math.random()*10000)
+      name: body.name,
+      number: body.number,
+//    id: Math.floor(Math.random()*10000)
     })
     person.save()
       .then(saved => res.json(saved.toJSON()))
       .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updated => res.json(updated.toJSON()))
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
